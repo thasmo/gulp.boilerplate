852191da39a0b01b5ab7c9cc1111ef98c36a1988
Thomas Deinhamer <thasmo@gmail.com>
2015-10-19 23:56:18 +0200

    Split up build task into sub-tasks

--- task/base/build.js
+++ task/base/build.js
@@ -4,6 +4,17 @@ var gulp = require('gulp');
 var sequence = require('run-sequence');
 var include = require('require-dir');
 
-gulp.task('build', function(callback) {
-	sequence('reset', 'setup', Object.keys(include('build/')), callback);
+// Base
+gulp.task('build', ['build:prepare'], function() {
+	return gulp.start('build:tasks');
+});
+
+// Prepare
+gulp.task('build:prepare', function(callback) {
+	sequence('reset', 'setup', callback);
+});
+
+// Tasks
+gulp.task('build:tasks', function(callback) {
+	sequence(Object.keys(include('build/')), callback);
 });
