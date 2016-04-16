07fd71c73b9b94f88df0c50b3825c2dc7f4a11ee
Thomas Deinhamer <thasmo@gmail.com>
2016-04-16 13:11:06 +0200

    Add data-sources for templating

--- gulpfile.js/task/build/templates.js
+++ gulpfile.js/task/build/templates.js
@@ -2,6 +2,7 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
+var include = require('require-dir');
 var config = require('../../config');
 var helper = require('../../helper');
 var path = require('../../path');
@@ -12,6 +13,9 @@ var task = function() {
 
 	return gulp.src(path.source.template + '*.jade')
 		.pipe($.plumber(helper.error))
+		.pipe($.data(function() {
+			return include('../../../source/data/');
+		}))
 		.pipe($.jade(config.plugin.jade))
 		.pipe($.if(helper.cli.optimize, $.htmlmin(config.plugin.htmlmin)))
 		.pipe(gulp.dest(path.public.template))
