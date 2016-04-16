3aa21d4efbb504d34cfc487f53a493336b317c27
Thomas Deinhamer <thasmo@gmail.com>
2016-04-16 13:09:21 +0200

    Configure source-maps for vendor scripts

--- gulpfile.js/task/build/script/vendor.js
+++ gulpfile.js/task/build/script/vendor.js
@@ -12,10 +12,12 @@ var task = function() {
 
 	return gulp.src(path.source.script + 'vendor.js')
 		.pipe($.plumber(helper.error))
-		.pipe($.include())
-		.pipe($.thasmo.modernizr(config.plugin.modernizr))
-		.pipe($.concat('vendor.js'))
-		.pipe($.if(helper.cli.optimize, $.uglify()))
+		.pipe($.sourcemaps.init())
+			.pipe($.include())
+			.pipe($.thasmo.modernizr(config.plugin.modernizr))
+			.pipe($.concat('vendor.js'))
+			.pipe($.if(helper.cli.optimize, $.uglify()))
+		.pipe($.sourcemaps.write('.', config.plugin.sourcemaps.write))
 		.pipe(gulp.dest(path.public.script))
 		.pipe(helper.success(task.displayName));
 };
